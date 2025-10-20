ae15b7b11ed2325ace149b6e55dd8432c02c790d
Sriram Ananth <149565123+SriramAnanth2912@users.noreply.github.com>
2025-10-20 23:24:53 +0530

    added signals in game perspective

--- using-signals/sprite_2d.gd
+++ using-signals/sprite_2d.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
 
+signal health_depleted ## signal to be emitted when health reaches 0
+signal health_changed(old,new) ## signal to get the old and new health values
+var health: int = 100 ## health of the player
 var speed: int = 400 ## speed of the sprite
 var angular_speed: float = PI ## angular speed of the sprite
 
@@ -13,6 +16,13 @@ func _process(delta: float) -> void:
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
+
+func take_damage(amount:int) -> void:
+	health -= amount
+	health_changed.emit(health+amount, health)
+	if health <= 0:
+		health = 0
+		health_depleted.emit()
 
 
 func _on_button_pressed() -> void:

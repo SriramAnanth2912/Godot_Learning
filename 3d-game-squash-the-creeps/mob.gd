bf73121a09cff29b61750df1d3b9cf4ea95b88db
Sriram Ananth <149565123+SriramAnanth2912@users.noreply.github.com>
2025-11-09 21:36:55 +0530

    commit

--- 3d-game-squash-the-creeps/mob.gd
+++ 3d-game-squash-the-creeps/mob.gd
@@ -1,25 +1,26 @@
 extends CharacterBody3D
 
 # Minimum speed of the mob in meters per second.
-@export var min_speed = 10
+@export var min_speed: int= 10
 # Maximum speed of the mob in meters per second.
-@export var max_speed = 18
+@export var max_speed: int = 18
 
 
-func _physics_process(_delta):
+func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 # This function will be called from the Main scene.
-func initialize(start_position, player_position):
+func initialize(start_position: Vector3, player_position: Vector3) -> void:
 	# We position the mob by placing it at start_position
 	# and rotate it towards player_position, so it looks at the player.
-	look_at_from_position(start_position, player_position, Vector3.UP)
+	var target: Vector3 = Vector3(player_position.x, start_position.y, player_position.z)
+	look_at_from_position(start_position, target, Vector3.UP)
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
 	
 	# We calculate a random speed (integer)
-	var random_speed = randi_range(min_speed, max_speed)
+	var random_speed: int = randi_range(min_speed, max_speed)
 	# We calculate a forward velocity that represents the speed.
 	velocity = Vector3.FORWARD * random_speed
 	# We then rotate the velocity vector based on the mob's Y rotation
--- 3d-game-squash-the-creeps/player.gd
+++ 3d-game-squash-the-creeps/player.gd
@@ -5,12 +5,12 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration: float = 75
 
-var target_velocity = Vector3.ZERO
+var target_velocity: Vector3= Vector3.ZERO
 
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	# We create a local variable to store the input direction.
-	var direction = Vector3.ZERO
+	var direction: Vector3 = Vector3.ZERO
 
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
